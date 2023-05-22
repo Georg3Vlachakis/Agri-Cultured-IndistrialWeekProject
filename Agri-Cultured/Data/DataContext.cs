875b0d4470583bff2094f2505d22a2d1ebfd1f81
GeorgeVlachakisUni <g.vlachakis@mc-class.gr>
2023-05-22 23:46:36 +0300

    ASP.NET Identity implementation.

---
+++ Agri-Cultured/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Agri_Cultured.Data
+{
+    public class DataContext : IdentityDbContext
+    {
+        public DataContext(DbContextOptions options) : base(options) 
+        {
+
+        }
+    }
+}
