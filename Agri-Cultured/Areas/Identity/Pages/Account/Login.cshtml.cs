346800e8c75c08383de63e89dbde4cd78ba41b3c
KitsatisV <vkitsatis@gmail.com>
2023-05-25 13:21:42 +0300

    Implemented Logout

--- Agri-Cultured/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Agri-Cultured/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -91,7 +91,7 @@ namespace Agri_Cultured.Areas.Identity.Pages.Account
                 ModelState.AddModelError(string.Empty, ErrorMessage);
             }
 
-            returnUrl ??= Url.Content("~/");
+            returnUrl ??= Url.Content("~/Dashboard");
 
             // Clear the existing external cookie to ensure a clean login process
             await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
@@ -103,7 +103,7 @@ namespace Agri_Cultured.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl ??= Url.Content("~/");
+            returnUrl ??= Url.Content("~/Dashboard");
 
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
 
