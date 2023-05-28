640b7f8e3ae7a304fad614543d7aa127b9db5ff4
KitsatisV <vkitsatis@gmail.com>
2023-05-28 08:47:25 +0300

    Diary/Search

---
+++ Agri-Cultured/Data/Search.cs
@@ -0,0 +1,16 @@
+﻿namespace Agri_Cultured.Data
+{
+    public class Search
+    {
+        public Search() { }
+
+        public Search(string date, string description)
+        {
+            Date = date;
+            Description = description;
+        }
+
+        public string Date { get; set; }
+        public string Description { get; set; }
+    }
+}
